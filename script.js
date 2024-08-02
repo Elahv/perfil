85a965832f668ddee432d82939204c9673fd1aad
H <164843658+Elahv@users.noreply.github.com>
2024-08-02 01:25:17 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+let carta_perfi = document.querySelector('.carta_perfi');
+let carta_boton = document.getElementById('carta_boton');
+carta_boton.addEventListener('click', ()=>{
+    carta_perfi.classList.toggle('animacion');
+});
